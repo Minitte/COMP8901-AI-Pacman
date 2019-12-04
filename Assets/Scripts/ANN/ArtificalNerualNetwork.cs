b6373e4c631d210863678ae7f7401073d7539217
Minitte <19943415+Minitte@users.noreply.github.com>
2019-12-03 22:04:06 -0800

    forward function

--- Assets/Scripts/ANN/ArtificalNerualNetwork.cs
+++ Assets/Scripts/ANN/ArtificalNerualNetwork.cs
@@ -11,7 +11,7 @@ namespace ANN
 
         private Neuron[] outputLayer { get { return neurons[layerCount - 1]; } }
 
-        ArtificalNerualNetwork(int[][] structure)
+        public ArtificalNerualNetwork(int[][] structure)
         {
             // must be atleast 2 layers
             UnityEngine.Debug.Assert(structure.Length >= 2);
@@ -36,7 +36,8 @@ namespace ANN
 
                     for (int w = 0; w < neurons[layer][n].weights.Length; w++)
                     {
-                        neurons[layer][n].weights[w] = (float)(rand.Next(0, 10) - 5) / 100f;
+                        //neurons[layer][n].weights[w] = (float)(rand.Next(0, 10) - 5) / 100f;
+                        neurons[layer][n].weights[w] = 0f;
                     }
                 }
             }
@@ -47,18 +48,71 @@ namespace ANN
             // input must be same length as input layer
             UnityEngine.Debug.Assert(input.Length == neurons[0].Length);
 
-            float[] results = new float[neurons[neurons.Length].Length];
+            float[][] results = CreateResultArray();
 
-            for (int layer = 0; layer < structure.Length; layer++)
+            results[0] = input;
+
+            // start at layer 0 (input)
+            for (int layer = 0; layer < neurons.Length - 1; layer++)
             {
+                float[] sum = new float[neurons[layer + 1].Length];
 
+                // input * weight for all neurons in current layer
+                for (int n = 0; n < neurons[layer].Length; n++)
+                {
+                    float[] weightedValues = neurons[layer][n].Process(results[layer][n]);
+
+                    for (int i = 0; i < sum.Length; i++)
+                    {
+                        sum[i] += weightedValues[i];
+                    }
+                }
+
+                // apply activation function
+                ApplyActivationFunction(sum, 1.0f);
+
+                results[layer + 1] = sum;
             }
-            return results;
+
+            return results[layerCount - 1];
         }
 
-        struct Neuron
+        private void ApplyActivationFunction(float[] arr, float upper)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > upper) arr[i] = 1;
+                else arr[i] = 0;
+            }
+        }
+
+        private float[][] CreateResultArray()
+        {
+            float[][] arr = new float[layerCount][];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = new float[neurons[i].Length];
+            }
+
+            return arr;
+        }
+
+        class Neuron
         {
             public float[] weights;
+
+            public float[] Process(float value)
+            {
+                float[] results = new float[weights.Length];
+
+                for (int i = 0; i < weights.Length; i++)
+                {
+                    results[i] = value * weights[i];
+                }
+
+                return results;
+            }
         }
     }
 
