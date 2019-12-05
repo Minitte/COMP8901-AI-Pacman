ca937b03e9e968a1bc02a1982cd76512875eb35a
Minitte <arcentalminitte@gmail.com>
2019-12-05 12:08:45 -0800

    Created NGram class

---
+++ Assets/Scripts/NGram/NGramString.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+
+namespace NGram
+{
+    public class NGramString
+    {
+        private const char COMBO_SPILTER = '/';
+        private Dictionary<string, NGramSet> m_combos;
+
+        private int m_total;
+
+        NGramString()
+        {
+            m_combos = new Dictionary<string, NGramSet>();
+        }
+
+        public void Add(string before, string after)
+        {
+            string combo = before + COMBO_SPILTER + after;
+
+            if (m_combos.ContainsKey(combo))
+            {
+                m_combos[combo].Add(after);
+            }
+            else
+            {
+                m_combos.Add(combo, new NGramSet(before));
+            }
+
+            m_total++;
+        }
+
+        public string Predict(string before)
+        {
+            string best_before = "";
+            int best_matchLength = 0;
+
+            foreach (string s in m_combos.Keys)
+            {
+                // does not apply so skip.
+                if (before.StartsWith(s)) continue;
+
+                // current best is more specific
+                if (best_matchLength > s.Length) continue;
+
+                best_before = m_combos[s].GetHighestProp();
+                best_matchLength = s.Length;
+            }
+
+            return best_before;
+        }
+
+        class NGramSet
+        {
+            public string before;
+
+            private List<string> afterList;
+
+            private List<int> countList;
+
+            public NGramSet(string before)
+            {
+                this.before = before;
+
+                afterList = new List<string>();
+                countList = new List<int>();
+            }
+
+            public void Add(string after)
+            {
+                if (afterList.Contains(after))
+                {
+                    int index = afterList.IndexOf(after);
+                    countList[index] = countList[index] + 1;
+                }
+                else
+                {
+                    afterList.Add(after);
+                    countList.Add(1);
+                }
+            }
+
+            public string GetHighestProp()
+            {
+                string best_after = afterList[0];
+                float best_prop = (float)countList[0] / (float)countList.Count;
+
+                for (int i = 0; i < afterList.Count; i++)
+                {
+                    float prop = (float)countList[i] / (float)countList.Count;
+
+                    if (prop > best_prop)
+                    {
+                        best_prop = prop;
+                        best_after = afterList[i];
+                    }
+                }
+
+                return best_after;
+            }
+        }
+    }
+}
