967cfb4ea376667da13b703bf486c813e34a96f0
Minitte <19943415+Minitte@users.noreply.github.com>
2019-12-05 19:05:17 -0800

    Button scene changers

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void ChangeScene(string sceneName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+}
